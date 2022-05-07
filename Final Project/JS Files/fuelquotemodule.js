c572dc1692746b57b8ca834c238b28e21a96656a
victorchkwok <61713215+victorchkwok@users.noreply.github.com>
2022-05-06 21:20:39 -0500

    Add files via upload

--- Final Project/JS Files/fuelquotemodule.js
+++ Final Project/JS Files/fuelquotemodule.js
@@ -1,27 +1,30 @@
-
 var pg = require('pg');
 var conString = "postgres://zjfheujb:zfGcU3cxrp-x5qS9hT7AgsFkToAvUPcs@ruby.db.elephantsql.com/zjfheujb"
-var client = new pg.Client(conString);
-const ordergallon = document.getElementById("gallon");
-const orderprice = document.getElementById("price");
+const client = new pg.Client(conString);
+const form = document.getElementById('form')
+const ordernum = 6066699;
+const username = 'username123';
+const gallonr= document.getElementById("gallon");
+const addr = document.getElementById("place");
 const ddate = document.getElementById("ddate");
-const place = document.getElementById("place");
-var currentorderNum = 1005;
+const price = document.getElementById("price");
+
 
 
 async function main(){
-    await client.connect();
+    await client.connect()
     form.addEventListener('submit', (e) => {
-    
-    let db_user = await client.query(`
+        try{
+            await client.query(`
             INSERT INTO fuel_quote_history(order_id, user_id, gallons_requested, delivery_address, delivery_date, price)
-            VALUES ('${currentorderNum.value}', '${localStorage.username},'${ordergallon.value}','${place.value}','${ddate.value}','${orderprice.value}');
-            `);
-            currentorderNum++;
-          
-        })
-    client.end();
-
-    }
+            VALUES (`+ordernum+`,'`+username+`',`+gallonr+`, '`+addr+`', '`+ddate+`', `+price+`);
+            `)
+        }
+        catch(err){
+            console.log(err)
+        }
+    })
+    client.end()
+}
 
 main();
